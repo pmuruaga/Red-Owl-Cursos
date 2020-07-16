470f4b171d77533b2a2e945ed276ebf4d3b76cd8
PabloMur <pmuruaga@hotmail.com>
2020-07-15 21:07:54 -0300

    Lito el registro de usuarios en el back.

---
+++ ProyectoCore/Aplicacion/Seguridad/Registrar.cs
@@ -0,0 +1,86 @@
+﻿using Aplicacion.Contratos;
+using Aplicacion.ManejadorError;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Seguridad
+{
+    public class Registrar
+    {
+        public class Ejecuta : IRequest<UsuarioData> {
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string Username { get; set; }
+        }
+
+        public class EjecutaValidador : AbstractValidator<Ejecuta> {
+            public EjecutaValidador()
+            {
+                RuleFor(x => x.Nombre).NotEmpty();
+                RuleFor(x => x.Apellidos).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Username).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, UsuarioData>
+        {
+            private readonly CursosOnlineContext _context;
+            private readonly UserManager<Usuario> _userManager;
+            private readonly IJwtGenerador _jwtGenerador;
+            public Manejador(CursosOnlineContext context, UserManager<Usuario> userManager, IJwtGenerador jwtGenerador) {
+                _context = context;
+                _userManager = userManager;
+                _jwtGenerador = jwtGenerador;
+            }
+            public async Task<UsuarioData> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var existe = await _context.Users.Where(x => x.Email == request.Email).AnyAsync();
+                if (existe) {
+                    throw new ManejadorExcepcion(HttpStatusCode.BadRequest, new { mensaje = "Existe ya un usuario registrado con ese email" });
+                }
+
+                var existeUsername = await _context.Users.Where(x => x.UserName == request.Username).AnyAsync();
+                if (existeUsername)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.BadRequest, new { mensaje = "Existe ya un usuario registrado con ese username" });
+                }
+
+                var usuario = new Usuario
+                {
+                    NombreCompleto = request.Nombre + " " + request.Apellidos,
+                    Email = request.Email,
+                    UserName = request.Username
+                };
+
+                var resultado = await _userManager.CreateAsync(usuario, request.Password);
+                if (resultado.Succeeded)
+                {
+                    return new UsuarioData
+                    {
+                        NombreCompleto = usuario.NombreCompleto,
+                        Token = _jwtGenerador.CrearToken(usuario),
+                        Username = usuario.UserName,
+                        Email = usuario.Email
+                    };
+                }
+
+                throw new Exception("No se pudo crear el usuario");
+            }
+        }
+    }
+}
--- ProyectoCore/WebAPI/Controllers/UsuarioController.cs
+++ ProyectoCore/WebAPI/Controllers/UsuarioController.cs
@@ -18,5 +18,11 @@ namespace WebAPI.Controllers
         {
             return await Mediator.Send(parametros);
         }
+
+        // http://localhost:5000/api/Usuario/registrar
+        [HttpPost("registrar")]
+        public async Task<ActionResult<UsuarioData>> Registrar(Registrar.Ejecuta parametros) {
+            return await Mediator.Send(parametros);
+        }
     }
 }
