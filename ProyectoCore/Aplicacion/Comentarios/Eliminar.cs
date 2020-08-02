ac8f3f5c9863ad157045a9053ec4b1ec7cbf5f8f
PabloMur <pmuruaga@hotmail.com>
2020-08-02 16:13:13 -0300

    Adding functionality to manage comments: New and Delete Comment.

---
+++ ProyectoCore/Aplicacion/Comentarios/Eliminar.cs
@@ -0,0 +1,46 @@
+﻿using Aplicacion.ManejadorError;
+using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Comentarios
+{
+    public class Eliminar
+    {
+        public class Ejecuta : IRequest {
+            public Guid ComentarioId { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+
+            public Manejador(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var comentario = await _context.Comentario.FindAsync(request.ComentarioId);
+
+                if(comentario == null)
+                {
+                    throw new ManejadorExcepcion(System.Net.HttpStatusCode.NotFound, new { mensaje = "No se encontro el comentario a borrar" });
+                }
+                _context.Remove(comentario);
+                var resultado = await _context.SaveChangesAsync();
+                if (resultado > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo eliminar el comentario");
+            }
+        }
+    }
+}
---
+++ ProyectoCore/Aplicacion/Comentarios/Nuevo.cs
@@ -0,0 +1,64 @@
+﻿using Dominio;
+using FluentValidation;
+using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Comentarios
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Alumno { get; set; }
+            public int Puntaje { get; set; }
+            public string Comentario { get; set; }
+            public Guid CursoId { get; set; }
+        }
+
+        public class EjecutaValidacion : AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidacion()
+            {
+                RuleFor(x => x.Alumno).NotEmpty();
+                RuleFor(x => x.Puntaje).NotEmpty();
+                RuleFor(x => x.Comentario).NotEmpty();
+                RuleFor(x => x.CursoId).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _cursosOnlineContext;
+            public Manejador(CursosOnlineContext cursosOnlineContext)
+            {
+                _cursosOnlineContext = cursosOnlineContext;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var comentario = new Comentario
+                {
+                    ComentarioId = Guid.NewGuid(),
+                    Alumno = request.Alumno,
+                    Puntaje = request.Puntaje,
+                    ComentarioTexto = request.Comentario,
+                    CursoId = request.CursoId
+                };
+
+                _cursosOnlineContext.Comentario.Add(comentario);
+
+                var resultado = await _cursosOnlineContext.SaveChangesAsync();
+                if(resultado > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el comentario");
+            }
+        }
+    }
+}
