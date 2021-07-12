158fdf3bce638ea2795243b09e50f507ab810a50
Pablo Muruaga <pmuruaga@hotmail.com>
2021-07-11 22:05:47 -0300

    Agregados Menues Derecho e Izquierdo

--- Front/cursos-online-app/src/componentes/navegacion/bar/BarSesion.js
+++ Front/cursos-online-app/src/componentes/navegacion/bar/BarSesion.js
@@ -1,59 +1,137 @@
-import { IconButton, Toolbar, Typography, makeStyles, Button, Avatar } from '@material-ui/core';
-import React from 'react';
-import { useStateValue } from '../../../contexto/store';
+import {
+  IconButton,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  Avatar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import { useStateValue } from "../../../contexto/store";
 import FotoUsuarioTemp from "../../../logo.svg";
+import { MenuIzquierda } from "./menuIzquierda";
+import { MenuDerecha } from "./menuDerecha";
 
 const useStyles = makeStyles((theme) => ({
-    seccionDesktop : {
-        display: "none",
-        [theme.breakpoints.up("md")] : {
-            display: "flex"
-        }
+  seccionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
-    seccionMobile : {
-        display: "flex",
-        [theme.breakpoints.up("md")] : {
-            display: "none"
-        }
+  },
+  seccionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
-    grow : {
-        flexGrow: 1
-    },
-    avatarSize : {
-        width: 40,
-        height: 40
-    }
-}))
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  avatarSize: {
+    width: 40,
+    height: 40,
+  },
+  list: {
+    width: 250,
+  },
+  listItemText : {
+      fontSize : "14px",
+      fontWeight: 600,
+      paddingLeft: "15px",
+      color: "#212121"
+  }
+}));
 
-const BarSesion = () => {
-    const classes = useStyles();
+const BarSesion = (props) => {
+  const classes = useStyles();
 
-    /* Declaro una constante del tipo useStateValue (se importa), este tiene la variable de 
+  /* Declaro una constante del tipo useStateValue (se importa), este tiene la variable de 
         sesión global sesionUsuario. Esta esta definida en el index.js de los reducers.
         Esto es lo que me va permitir consumir los datos de la variable de sesión. */
-    const [{sesionUsuario}, dispatch] = useStateValue();
+  const [{ sesionUsuario }, dispatch] = useStateValue();
+  const [abrirMenuIzquierda, setAbrirMenuIzquierda] = useState(false);
+  const [abrirMenuDerecha, setAbrirMenuDerecha] = useState(false);
 
-    return (
-        <Toolbar>
-            <IconButton color="inherit">
-                <i className="material-icons">menu</i>
-            </IconButton>
-            
-            <Typography variant="h6">Cursos Online</Typography>
+  const cerrarMenuIzquierda = () => {
+    setAbrirMenuIzquierda(false);
+  }
 
-            <div class={classes.grow}></div>
-            <div class={classes.seccionDesktop}>
-                <Button color="inherit">Salir</Button>
-                <Button color="inherit">{sesionUsuario ? sesionUsuario.usuario.nombreCompleto : ""}</Button>
-                <Avatar src={FotoUsuarioTemp}></Avatar>
-            </div>
-            <div class={classes.seccionMobile}>
-                <IconButton color="inherit">
-                    <i className="material-icons">more_vert</i>
-                </IconButton>
-            </div>
-        </Toolbar>
-    );
+  const abrirMenuIzquierdaAction = () => {
+    setAbrirMenuIzquierda(true);
+  }
+
+  const cerrarMenuDerecha = () => {
+    setAbrirMenuDerecha(false);
+  }
+
+  const salirSesionApp = () => {
+    localStorage.removeItem('token_seguridad');
+    props.history.push('/auth/login');
+  }
+
+  const abrirMenuDerechaAction = () => {
+    setAbrirMenuDerecha(true);
+  }
+
+  return (
+    <React.Fragment>
+      <Drawer
+        open={abrirMenuIzquierda}
+        onClose={cerrarMenuIzquierda}
+        anchor="left"
+      >
+        <div className={classes.list} onKeyDown={cerrarMenuIzquierda} onClick={cerrarMenuIzquierda}>
+          <MenuIzquierda classes={classes}></MenuIzquierda>
+        </div>
+      </Drawer>
+
+      <Drawer
+        open={abrirMenuDerecha}
+        onClose={cerrarMenuDerecha}
+        anchor="right"
+      >
+        <div 
+          role="button" 
+          onClick={cerrarMenuDerecha} 
+          onKeyDown={cerrarMenuDerecha}
+        >
+          <MenuDerecha 
+            classes={classes} 
+            salirSesion={salirSesionApp} 
+            usuario={sesionUsuario ? sesionUsuario.usuario : null} 
+            />
+        </div>
+      </Drawer>
+
+      <Toolbar>
+        <IconButton color="inherit" onClick={abrirMenuIzquierdaAction}>
+          <i className="material-icons">menu</i>
+        </IconButton>
+
+        <Typography variant="h6">Cursos Online</Typography>
+
+        <div class={classes.grow}></div>
+        <div class={classes.seccionDesktop}>
+          <Button color="inherit">Salir</Button>
+          <Button color="inherit">
+            {sesionUsuario ? sesionUsuario.usuario.nombreCompleto : ""}
+          </Button>
+          <Avatar src={FotoUsuarioTemp}></Avatar>
+        </div>
+        <div class={classes.seccionMobile}>
+          <IconButton color="inherit" onClick={abrirMenuDerechaAction}>
+            <i className="material-icons">more_vert</i>
+          </IconButton>
+        </div>
+      </Toolbar>
+    </React.Fragment>
+  );
 };
 
-export default BarSesion;
+export default withRouter(BarSesion);
---
+++ Front/cursos-online-app/src/componentes/navegacion/bar/menuDerecha.js
@@ -0,0 +1,22 @@
+import { List, ListItem, ListItemText, Avatar } from '@material-ui/core';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FotoUsuarioTemp from "../../../logo.svg";
+
+export const MenuDerecha = ({
+    classes,
+    usuario,
+    salirSesion
+}) => (
+    <div className={classes.list}>
+        <List>
+            <ListItem button component={ Link }>
+                <Avatar src={usuario.foto || FotoUsuarioTemp} />
+                <ListItemText classes={{primary : classes.listItemText}} primary={usuario ? usuario.nombreCompleto : "" } />
+            </ListItem>
+            <ListItem button onClick={salirSesion}>                
+                <ListItemText classes={{primary : classes.listItemText}} primary = "Salir" />
+            </ListItem>
+        </List>
+    </div>
+);
